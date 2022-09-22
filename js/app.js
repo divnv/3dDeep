817d9726aface46d10fbe1b9118490ecb9a330ae
Diana Gusewa <diany.gusewa@yandex.ru>
2022-09-22 18:16:10 +0300

    Finished first frame

--- js/app.js
+++ js/app.js
@@ -1,4 +1,28 @@
 // 3D Scroll
 
-let zSpacing = -1000
-let lastPos = zSpacing / 5
+let zSpacing = -1000,
+	lastPos = zSpacing / 5,
+	frames = Array.from(document.getElementsByClassName('frame')),
+	zVals = []
+
+window.onscroll = handleScroll
+
+handleScroll()
+
+function handleScroll() {
+	let top = document.documentElement.scrollTop,
+		delta = lastPos - top
+
+	lastPos = top
+
+	frames.forEach((n, i) => {
+		zVals.push(i * zSpacing + zSpacing)
+		zVals[i] += delta * -2
+
+		let frame = frames[i],
+			transform = `translateZ(${zVals[i]}px)`,
+			opacity = zVals[i] < Math.abs(zSpacing) / 1.5 ? 1 : 0
+
+		frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
+	})
+}
